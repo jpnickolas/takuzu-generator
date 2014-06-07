f3c8eff3a7f0742fbec4d58e5d7a776d1cc934ff
Nick Hilton <hilton@csh.rit.edu>
2014-06-07 23:21:34 +0100

    The generator now creates a randomly completed board, and validates it.

--- generator.cpp
+++ generator.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
+#include<stdlib.h>
+#include<time.h>
 
-#define SIZE 10
+#define SIZE 4
 
 using namespace std;
 
@@ -12,12 +14,84 @@ void printBoard(char board[][SIZE]) {
   }
 }
 
+bool validBoard(char board[][SIZE]) {
+
+  int zero=0, one=0;
+
+  for(int i=0; i<SIZE; i++) {
+    for(int j=0; j<SIZE; j++) {
+      if(board[i][j] == '1')
+        one++;
+      else
+        zero++;
+    }
+    if(one != zero)
+      return false;
+    
+    one = 0;
+    zero = 0;
+  }
+
+  for(int i=0; i<SIZE; i++) {
+    for(int j=0; j<SIZE; j++) {
+      if(board[j][i] == '1')
+        one++;
+      else
+        zero++;
+    }
+    if(one != zero)
+      return false;
+    
+    one = 0;
+    zero = 0;
+  }
+  
+  for(int i=0; i<SIZE; i++) {
+    for(int j=i+1; j<SIZE; j++) {
+      bool same = true;;
+      for(int k=0; k<SIZE; k++) {
+        if(board[i][k] != board[j][k]) {
+          same = false;
+          break;
+        }
+      }
+      if(same)
+        return false;
+    }
+  }
+
+  for(int i=0; i<SIZE; i++) {
+    for(int j=i+1; j<SIZE; j++) {
+      bool same = true;;
+      for(int k=0; k<SIZE; k++) {
+        if(board[k][i] != board[k][j]) {
+          same = false;
+          break;
+        }
+      }
+      if(same)
+        return false;
+    }
+  }
+
+  return true;
+}
+
 int main(void) {
   char board[SIZE][SIZE];
 
+  srand(time(NULL));
+  
   for(int i=0; i<SIZE; i++)
     for(int j=0; j<SIZE; j++)
-      board[i][j] = '_';
-
+      board[i][j] = rand()%2+48;
+  
   printBoard(board);
+
+  if(validBoard(board)) {
+    cout<<"Valid"<<endl;
+  }
+  else {
+    cout<<"Invalid"<<endl;
+  }
 }
