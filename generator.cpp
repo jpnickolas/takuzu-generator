50196ceea2140beee723740aa64114e5eee3a861
Nick Hilton <hilton@csh.rit.edu>
2014-06-14 17:50:09 +0100

    Added a few functions that will be used to actually generate the board.
    Not all functions required have been added. A function that actually
    solves the board is still required, but this will recursively find a
    board with only one solution.

--- generator.cpp
+++ generator.cpp
@@ -4,11 +4,14 @@
 #include<time.h>
 
 #define SIZE 4
+#define BLANK '/'
+#define ZERO '0'
+#define ONE '1'
 
 using namespace std;
 
 //Prints out the board separated by spaces
-void printBoard(vector<vector<char> > board) {
+void print_board(vector<vector<char> > board) {
   for(int i=0; i<board.size(); i++) {
     for(int j=0; j<board[i].size(); j++)
       cout<<board[i][j]<<' ';
@@ -17,16 +20,16 @@ void printBoard(vector<vector<char> > board) {
 }
 
 //Returns true if the board is a valid board, and false otherwise 
-bool validBoard(vector<vector<char> > board) {
+bool valid_board(vector<vector<char> > board) {
 
   int zero=0, one=0, size=board.size();
 
   //checks the rows for too many 1's or 0's
   for(int i=0; i<size; i++) {
     for(int j=0; j<size; j++) {
-      if(board[i][j] == '1')
+      if(board[i][j] == ONE)
         one++;
-      else if(board[i][j] == '0')
+      else if(board[i][j] == ZERO)
         zero++;
     }
     if(one > size/2 ||  zero > size/2)
@@ -41,7 +44,7 @@ bool validBoard(vector<vector<char> > board) {
     for(int j=0; j<size; j++) {
       if(board[j][i] == '1')
         one++;
-      else if(board[j][i] == '0')
+      else if(board[j][i] == ZERO)
         zero++;
     }
     if(one > size/2 ||  zero > size/2)
@@ -55,7 +58,7 @@ bool validBoard(vector<vector<char> > board) {
   bool consecutive = false;
   for(int i=0; i<size; i++) {
     for(int j=1; j<size; j++) {
-      if(board[i][j]==board[i][j-1] && board[i][j]!='/') {
+      if(board[i][j]==board[i][j-1] && board[i][j]!=BLANK) {
         if(consecutive)
           return false;
         else
@@ -70,7 +73,7 @@ bool validBoard(vector<vector<char> > board) {
   consecutive = false;
   for(int i=0; i<size; i++) {
     for(int j=1; j<size; j++) {
-      if(board[j][i]==board[j-1][i] && board[j][i]!='/') {
+      if(board[j][i]==board[j-1][i] && board[j][i]!=BLANK) {
         if(consecutive)
           return false;
         else
@@ -86,7 +89,7 @@ bool validBoard(vector<vector<char> > board) {
     for(int j=i+1; j<size; j++) {
       bool same = true;
       for(int k=0; k<size; k++) {
-        if(board[i][k] != board[j][k] || board[j][k]=='/') {
+        if(board[i][k] != board[j][k] || board[j][k]==BLANK) {
           same = false;
           break;
         }
@@ -101,7 +104,7 @@ bool validBoard(vector<vector<char> > board) {
     for(int j=i+1; j<size; j++) {
       bool same = true;
       for(int k=0; k<size; k++) {
-        if(board[k][i] != board[k][j] || board[k][j]=='/') {
+        if(board[k][i] != board[k][j] || board[k][j]==BLANK) {
           same = false;
           break;
         }
@@ -112,6 +115,67 @@ bool validBoard(vector<vector<char> > board) {
   }
 
   return true;
+}
+
+int count_blanks(vector<vector<char> > board) {
+  int blanks = 0;
+  
+  for(int i=0; i<board.size(); i++) {
+    for(int j=0; j<board[i].size(); j++) {
+      if(board[i][j] == BLANK) {
+        blanks++;
+      }
+    }
+  }
+}
+
+void get_random_blank(vector<vector<char> > board, int &x, int &y) {
+  int blanks = count_blanks(board);
+
+  int blank = rand()%blanks;
+
+  for(int i=0; i<board.size(); i++) {
+    for(int j=0; j<board[i].size(); j++) {
+      if(board[i][j]==BLANK) {
+        if(blank>0)
+          blank--;
+        else {
+          x=i;
+          y=j;
+          return;
+        }
+      }
+    }
+  }
+}
+
+vector<vector<char> > generate_puzzle(vector<vector<char> > board) {
+  vector<vector<char> > test_board = vector<vector<char> >(board);
+
+  int solutions = solve_puzzle(test_board);
+
+  if(solutions == 1) {
+    return board;
+  }
+  else if(solutions > 1) {
+    int x, y;
+    
+    get_random_blank(board, x, y);
+    
+    board[x][y] = rand()%2+48;
+    
+    vector<vector<char> > generated_puzzle = generate_puzzle(board);
+    if(generated_puzzle.size() == 0) {
+      if(board[x][y]=='0')
+        board[x][y]='1';
+      else
+        board[x][y]='0';
+      
+      return generate_puzzle(board);
+    }
+  }
+
+  return vector<vector<char> >(0);
 }
 
 int main(void) {
@@ -127,10 +191,10 @@ int main(void) {
       board[i][j] = rand()%3+47;
   
   //prints out the board
-  printBoard(board);
+  print_board(board);
 
   //validates the board
-  if(validBoard(board)) {
+  if(valid_board(board)) {
     cout<<"Valid"<<endl;
   }
   else {
